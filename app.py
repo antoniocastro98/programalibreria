4484e51ab575c16eb4b45cc3393ae6ca80c68a07
root <antoniocastro-@hotmail.com>
2021-04-19 17:54:57 +0200

    edición de los ficheros app.py, pagina principal y pagina de potencias.

--- app.py
+++ app.py
@@ -4,4 +4,24 @@ app = Flask (__name__)
 
 @app.route('/')
 def inicio():
-    return render_template("inicio.html")
+    return render_template("principal.html")
+
+
+@app.route('/potencia/<base>/<exponente>')
+def potencia(base, exponente):
+
+    if int(exponente) > 0:
+        resultado = int(base)**int(exponente)
+
+    elif int(exponente) == 0:
+        resultado = 1
+
+    elif int(exponente) < 0:
+        resultado = 1/(int(base)**int(exponente))
+    
+    else:
+        return abort(404)
+    
+    return render_template('potencia.html', resultado=resultado, base=base, exponente=exponente)
+
+app.run(debug=True)
